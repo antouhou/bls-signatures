a7fead85c225882cecbd8c7748822a0bc548b920
Anton Suprunchuk <antouhou@gmail.com>
2020-07-27 12:57:26 +0300

    Add karma reporter

--- js_build.sh
+++ js_build.sh
@@ -5,5 +5,5 @@ git submodule update --init --recursive
 mkdir js_build
 cd js_build
 
-cmake ../ -DCMAKE_TOOLCHAIN_FILE=$(realpath $(which emcc))/cmake/Modules/Platform/Emscripten.cmake
-cmake --build . --
+cmake ../ -DCMAKE_TOOLCHAIN_FILE=/Users/anton/Projects/emsdk/fastcomp/emscripten/cmake/Modules/Platform/Emscripten.cmake
+cmake --build . -- -j10
